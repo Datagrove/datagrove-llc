f6e76c6ad4c618aa3203dff5dc9d4f3909acf411
jim <jimh@datagrove.com>
2022-05-18 20:51:54 -0400

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 # Need to update this
 
-yarn docs:build && scp -i ~/.ssh/ls2b.pem -r docs/.vitepress/dist/* ubuntu@datagrove.com://home/ubuntu/datagrove
+yarn docs:build && scp -i ~/.ssh/ls2b.pem -r docs/.vitepress/dist/* ubuntu@datagrove.com://var/www/html
